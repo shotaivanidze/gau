8e209f12240384939d8aa82d7f60d90ba2cdc38f
LAB316 Student21 <LAB316_Student21@gau.local>
2019-09-30 14:41:42 +0400

    homework edit

---
+++ week_2/week 2_1/script.js
@@ -0,0 +1,15 @@
+function printHello(){
+    console.log("Hello, I am JavaScript");
+}
+
+function sum0fTwoNumbers(number1, number2){
+    console.log(number1+number2);
+}
+
+function getFive(){
+    return 5;
+}
+
+function returnSum0fTwoNumbers(number1, number2){
+    return number1+number2;
+}

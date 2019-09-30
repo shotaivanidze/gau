89ba90987c268dcb6dd5c0cddcfe88a34941d030
LAB316 Student21 <LAB316_Student21@gau.local>
2019-09-30 15:48:38 +0400

    classwork 2

--- week_3/main.js
+++ week_3/main.js
@@ -1,6 +1,11 @@
 function testNumber1(){
     rs = document.getElementById("result1");
     rs.innerHTML = Number.MAX_VALUE;
-    rs.innerHTML += " ";
+    rs.innerHTML += "</br>";
     rs.innerHTML += Number.MIN_VALUE;
 }
+
+function testNumber2(){
+    return (Math.round(Math.random()*11));
+}
+
